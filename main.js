f69fc94e5129b5a52baa701b5ae2c1a652da7306
Md. Raian Hossain <raiansanil@gmail.com>
2024-10-29 20:25:56 +0600

    again updated cors policy

--- main.js
+++ main.js
@@ -8,20 +8,12 @@ const { connectDb } = require("./database/connect");
 const app = express();
 const router = express.Router();
 // app.use(cors({ credentials: true, origin: true }));
-app.use(cors({ credentials: true, origin: "https://connect-me-iota.vercel.app" }));
+app.use(cors({ credentials: true, origin: "*" }));
 
 app.use(express.static(__dirname + "/public"));
 app.use("/uploads", express.static("uploads"));
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://connect-me-iota.vercel.app");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  if (req.method === "OPTIONS") {
-    return res.status(200).end();
-  }
-  next();
-});
+
 
 // /!\ Bind the router db to the app
 
